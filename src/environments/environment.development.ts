c88979ff274fe5e8c49d505c1cc858a8e833e5f5
Parrot Live user <user@localhost.localdomain>
2026-07-15 13:53:35 +0000

    gotenmber ok

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     pocketbaseUrl: 'https://db.buckapi.online:8095/',
-    gotenbergBaseUrl: 'https://gotenberg.appverificar.online//',
+    gotenbergBaseUrl: 'https://gotenberg.appverificar.online/',
     // gotenbergBaseUrl: 'https://gotenberg.appverificar.online/',
     gotenbergUsername: 'tremaine.bogisich-reichert',
     gotenbergPassword: 'zdbdb0zoq2nqy73t',
